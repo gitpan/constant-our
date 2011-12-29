8d7b74ed4e6e0d2137a75fa754981e2a466d0af1
Evdokimov Denis <green@cpan.org>
2011-12-29 20:35:47 +0000

    Import of GREEN/constant-our-0.04 from CPAN.
    gitpan-cpan-distribution: constant-our
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         GREEN/constant-our-0.04.tar.gz
    gitpan-cpan-author:       GREEN
    gitpan-cpan-maturity:     released

--- lib/constant/our.pm
+++ lib/constant/our.pm
@@ -9,10 +9,10 @@ constant::our - Perl pragma to declare constants like our vars
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use constant;
 use Exporter;
